3df552af126c3391c1751f66195e742aec08e5db
l0kt33n <aa77855@gmail.com>
2019-04-21 21:26:27 -0700

    Finished write up

--- inaugural/hw1.py
+++ inaugural/hw1.py
@@ -1,11 +1,12 @@
 import nltk
-import string
 
+#process_wc_top_avglen(filename)
+#This function takes in the filename, and returns the total word counts, NLTK
+#frequency distribution, and the average sentence length as an array
 def process_wc_top_avglen(filename):
     with open(filename) as f:
         data = f.read()
 
-    
     sents = nltk.sent_tokenize(data)
     tokens = nltk.word_tokenize(data)
     words = [word for word in tokens if word.isalpha()]
@@ -17,6 +18,10 @@ def process_wc_top_avglen(filename):
 
     return (total_words, fd, avg_sent_len)
 
+#print_wc_top_avglen(filename)
+#This function takes in the filename, calls process_wc_top_avglen(filename)
+#and prints the filename, total wordcount, the freqdist, and average sentence
+#length with some formatting
 def print_wc_top_avglen(filename):
     result = process_wc_top_avglen(filename)
     avg_len = round(result[2], 2)
